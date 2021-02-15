3c08eb0927943b07a3d0783933a88f7dbc01cbdb
martincnk <79065789+martincnk@users.noreply.github.com>
2021-02-14 19:17:29 -0500

    Create surfaces.r

---
+++ surfaces.r
@@ -0,0 +1,19 @@
+surface_square <- function(a) {
+  b <- a ^ 2
+}
+surface_paraleograme <- function(a, b) {
+  a * b
+}
+
+surface_triangle <- function(a, b) {
+  a * b / 2
+}
+surface_regular_polygone <- function(a, b, c) {
+  a * b * c
+}
+surface_trapeze <- function(a, b, c) {
+  ((a + b) * c) / 2
+}
+surface_diamond <- function(a, b) {
+  a * b / 2
+}
